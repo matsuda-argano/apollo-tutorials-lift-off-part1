598fec3c1385cce9b68a77a5426a09a3a1cbfb90
matsuda-argano <matsuda@argano.co.jp>
2022-07-25 21:00:34 +0900

    useQuery

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { gql } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
 
 import { Layout } from '../components';
+import TrackCard from '../containers/track-card';
+import { QueryResult } from '../components';
 
 const TRACKS = gql`
   query GetTracks {
@@ -24,7 +26,17 @@ const TRACKS = gql`
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
